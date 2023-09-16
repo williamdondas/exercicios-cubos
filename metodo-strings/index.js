3b33f1595ec68632f1ca30317dbd245929d143f9
williamdondas <williamdondas@hotmail.com>
2023-09-16 17:32:37 -0300

    adicionado ex 01

---
+++ metodo-strings/index.js
@@ -0,0 +1,5 @@
+const comentario = "Esse COVID é muito perigoso!";
+
+if (comentario.includes("COVID") || comentario.includes("Covid") ){
+    console.log("Comentário bloqueado por conter palavras proibidas")
+}
